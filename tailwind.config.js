c51390d85f7aeaa092f7875e85ef5329430c2e15
vickyjha23 <vickyjha388@gamil.com>
2024-07-07 13:56:53 +0530

    comify code is getting pushed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+       fontFamily: {
+           "kaushan-script" : [ "Kaushan Script", "cursive"]
+       }
+    },
+  },
+  plugins: [],
+}
+
